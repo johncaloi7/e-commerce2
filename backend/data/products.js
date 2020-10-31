9b31d8a24f76d39563d838c7c0a5df60205a3c69
unknown <pinlac.johncarlo@gmail.com>
2020-10-31 13:02:40 +0800

    initial deployment of e-commerce

---
+++ backend/data/products.js
@@ -0,0 +1,75 @@
+const products = [
+  {
+    name: 'Sample 1',
+    image: '/images/sample.jpg',
+    description:
+    'Dolore adipisicing minim labore eu aliqua quis.',
+    brand: 'Sample',
+    category: 'Sample',
+    price: 89.99,
+    countInStock: 3,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Sample 2',
+    image: '/images/sample.jpg',
+    description:
+    'Dolore adipisicing minim labore eu aliqua quis.',
+    brand: 'Sample',
+    category: 'Sample',
+    price: 599.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Sample 3',
+    image: '/images/sample.jpg',
+    description:
+    'Dolore adipisicing minim labore eu aliqua quis.',
+    brand: 'Sample',
+    category: 'Sample',
+    price: 929.99,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Sample 4',
+    image: '/images/sample.jpg',
+    description:
+    'Dolore adipisicing minim labore eu aliqua quis.',
+    brand: 'Sample',
+    category: 'Sample',
+    price: 399.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Sample 5',
+    image: '/images/sample.jpg',
+    description:
+    'Dolore adipisicing minim labore eu aliqua quis.',
+    brand: 'Sample',
+    category: 'Sample',
+    price: 49.99,
+    countInStock: 7,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Sample 6',
+    image: '/images/sample.jpg',
+    description: 'Dolore adipisicing minim labore eu aliqua quis.',
+    brand: 'Sample',
+    category: 'Sample',
+    price: 29.99,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+]
+
+export default products
